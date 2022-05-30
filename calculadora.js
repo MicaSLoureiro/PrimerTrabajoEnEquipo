b39a6a9546bb8159fdd2902de81961ae68ff715d
MicaSLoureiro <micaela_loureiro@hotmail.com>
2022-05-30 17:58:40 -0300

    Agrego en calculadora process y un switch para ejecutar la app

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,25 @@
-let {} = require('./resta')
-let {} = require('./suma')
+let suma = require('./resta')
+let resta = require('./suma')
+
+let process = require('process');
+let operacion = process.argv[2]
+let num1 = process.argv[3]
+let num2 = process.argv[4]
+
+
+num1 = +num1
+num2 = +num2
+
+
+
+switch (operacion) {
+     case 'suma':
+         console.log(suma(num1, num2))
+        break;
+    case 'resta':
+        console.log(resta(num1, num2))
+           break;
+    default: 
+    console.log('No se encuentra la operación solicitada');
+        break;
+}
